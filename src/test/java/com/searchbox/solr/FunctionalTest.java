9532cb1dda45b921e20ec796d75e8f66c5490a30
Stephane Gamard <stephane.gamard@searchbox.com>
2012-10-17 19:39:14 +0200

    Added solr_empty and solr_data as 2 distinct sold setup for Andrew.

--- src/test/java/com/searchbox/solr/FunctionalTest.java
+++ src/test/java/com/searchbox/solr/FunctionalTest.java
@@ -4,6 +4,7 @@
  */
 package com.searchbox.solr;
 
+import java.io.IOException;
 import junit.framework.TestCase;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrResponse;
@@ -11,17 +12,18 @@ import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.core.CoreContainer;
 
 /**
  *
  * @author gamars
  */
-public class MainTest extends TestCase {
+public class FunctionalTest extends TestCase {
 
     SolrServer server;
 
-    public MainTest(String testName) {
+    public FunctionalTest(String testName) {
         super(testName);
     }
 
@@ -29,7 +31,7 @@ public class MainTest extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
 
-        System.setProperty("solr.solr.home", "./src/test/resources/solr_data");
+        System.setProperty("solr.solr.home", "./src/test/resources/solr_empty");
         CoreContainer.Initializer initializer = new CoreContainer.Initializer();
         CoreContainer coreContainer = initializer.initialize();
         this.server = new EmbeddedSolrServer(coreContainer, "pubmed");
@@ -38,22 +40,27 @@ public class MainTest extends TestCase {
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
+        server.deleteByQuery("*:*");
+        server.commit();
         server.shutdown();
     }
 
     // TODO add test methods here. The name must begin with 'test'. For example:
     // public void testHello() {}
-    public void testMain() throws SolrServerException {
-        SolrQuery query = new SolrQuery("*");
+    public void testMain() throws SolrServerException, IOException {
+
+
+        SolrInputDocument doc = new SolrInputDocument();
+        doc.addField("id", System.currentTimeMillis() + "");
+        doc.addField("content_srch", "Hello World");
+
+        server.add(doc);
+        server.commit();
+
+        SolrQuery query = new SolrQuery("hello");
         query.setParam("defType", "sense");
         QueryResponse response = server.query(query);
-        assertTrue("Query has no results!", response.getResults().getNumFound() == 45);
-    }
-    
-    public void testSenseQuery() throws SolrServerException {
-        SolrQuery query = new SolrQuery("information");
-        query.setParam("defType", "sense");
-        QueryResponse response = server.query(query);
-        assertTrue("Query has no results!", response.getResults().getNumFound() == 45);
+        
+        assertTrue("Query has no results!", response.getResults().getNumFound() == 1);
     }
 }
