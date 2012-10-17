dcc3b61cf941fe34cd0f0c34b9762b1876e5ea3b
Stephane Gamard <stephane.gamard@searchbox.com>
2012-10-17 21:03:41 +0200

    Added SenseParam class

---
+++ src/main/java/com/searchbox/commons/params/SenseParams.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.searchbox.commons.params;
+
+/**
+ *
+ * @author gamars
+ */
+public class SenseParams {
+    
+    public static String SENSE_WEIGHT = "sw";
+    
+    public static double DEFAULT_SENSE_WEIGHT = 1.0;
+}
