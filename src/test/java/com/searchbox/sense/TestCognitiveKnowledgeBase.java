9177648e80676ad38a3a6ef4fd67a559b4fa75b4
Stephane Gamard <stephane.gamard@searchbox.com>
2012-10-18 16:41:57 +0200

    Added LB support for SolrServers

--- src/test/java/com/searchbox/sense/TestCognitiveKnowledgeBase.java
+++ src/test/java/com/searchbox/sense/TestCognitiveKnowledgeBase.java
@@ -38,6 +38,7 @@ import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.search.SolrIndexSearcher;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -47,15 +48,20 @@ import org.junit.Test;
  */
 public class TestCognitiveKnowledgeBase extends EmptySolrTestCase {
 
-    private CognitiveKnowledgeBase ckb;
-    private SenseQuery sensequery;
-    private SenseScoreProvider sensescoreprovider;
-    
+    private static CognitiveKnowledgeBase CKB;
 
-    @Ignore
+    @BeforeClass
+    public static void setUp() throws Exception {
+        TestCognitiveKnowledgeBase.CKB = CognitiveKnowledgeBase.loadSparseCKB("test",
+                "./src/test/resources/CKB/pubmed/", "pubmed.cache",
+                "pubmed.idflog", "pubmed.tdic", 1.0, 1.0);
+    }
+
+    @Test
     public void testLoadingCKB() {
-        assertTrue("No terms has been loaded", ckb.getTerms().size() > 0);
-        assertTrue("CKB has no dimensions", ckb.getDimentionality() > 0);
+        assertNotNull(CKB);
+        assertTrue("No terms has been loaded", CKB.getTerms().size() > 0);
+        assertTrue("CKB has no dimensions", CKB.getDimentionality() > 0);
     }
 
     @Test
@@ -66,7 +72,7 @@ public class TestCognitiveKnowledgeBase extends EmptySolrTestCase {
         doc.addField("content_srch", "Hello World");
         solrServer.add(doc);
         solrServer.commit();
-        
+
         doc = new SolrInputDocument();
         id = System.currentTimeMillis() + "";
         doc.addField("id", id);
@@ -76,86 +82,51 @@ public class TestCognitiveKnowledgeBase extends EmptySolrTestCase {
 
         LOGGER.info("Getting TF for all document (q=*:*)");
         Map<String, HashMap<String, Integer>> tfms = SolrUtils.getTermFrequencyMapForQuery(solrServer, "content_srch", "*:*");
-        for(String sid:tfms.keySet()){
-            LOGGER.info("TF for document id#"+sid);
-            for(Entry<String, Integer> tf:tfms.get(sid).entrySet()){
-                LOGGER.info("\t"+tf.getKey()+"\t"+tf.getValue());
+        for (String sid : tfms.keySet()) {
+            LOGGER.info("TF for document id#" + sid);
+            for (Entry<String, Integer> tf : tfms.get(sid).entrySet()) {
+                LOGGER.info("\t" + tf.getKey() + "\t" + tf.getValue());
             }
         }
-        
-        
-        LOGGER.info("Getting TF for ONE document (q=id:"+id+")");
-        tfms = SolrUtils.getTermFrequencyMapForQuery(solrServer, "content_srch", "id:"+id);
-        for(String sid:tfms.keySet()){
-            LOGGER.info("TF for document id#"+sid);
-            for(Entry<String, Integer> tf:tfms.get(sid).entrySet()){
-                LOGGER.info("\t"+tf.getKey()+"\t"+tf.getValue());
+
+
+        LOGGER.info("Getting TF for ONE document (q=id:" + id + ")");
+        tfms = SolrUtils.getTermFrequencyMapForQuery(solrServer, "content_srch", "id:" + id);
+        for (String sid : tfms.keySet()) {
+            LOGGER.info("TF for document id#" + sid);
+            for (Entry<String, Integer> tf : tfms.get(sid).entrySet()) {
+                LOGGER.info("\t" + tf.getKey() + "\t" + tf.getValue());
             }
         }
-        
+
         LOGGER.info("TF for raw content for a CONTENT_SRCH field in schema");
         HashMap<String, Integer> tfs = SolrUtils.getTermFrequencyMapForContent(solrServer, "content_srch", "I like this method very much and will do!");
-        for(Entry<String, Integer> tf:tfs.entrySet()){
-            LOGGER.info("\t"+tf.getKey()+"\t"+tf.getValue());
+        for (Entry<String, Integer> tf : tfs.entrySet()) {
+            LOGGER.info("\t" + tf.getKey() + "\t" + tf.getValue());
         }
-        
+
         LOGGER.info("TF for raw content for a CATEGORY field in schema");
         tfs = SolrUtils.getTermFrequencyMapForContent(solrServer, "category", "I like this method very much!");
-        for(Entry<String, Integer> tf:tfs.entrySet()){
-            LOGGER.info("\t"+tf.getKey()+"\t"+tf.getValue());
+        for (Entry<String, Integer> tf : tfs.entrySet()) {
+            LOGGER.info("\t" + tf.getKey() + "\t" + tf.getValue());
         }
-        
+
         LOGGER.info("TF for raw content for a BODY field in schema");
         tfs = SolrUtils.getTermFrequencyMapForContent(solrServer, "body", "I like this method very much!");
-        for(Entry<String, Integer> tf:tfs.entrySet()){
-            LOGGER.info("\t"+tf.getKey()+"\t"+tf.getValue());
+        for (Entry<String, Integer> tf : tfs.entrySet()) {
+            LOGGER.info("\t" + tf.getKey() + "\t" + tf.getValue());
         }
-        
-        
-        
-        
-//        
-//        SolrQuery query = new SolrQuery("hello");
-//        QueryResponse response = solrServer.query(query);
-//        assertTrue("Query has no results!", response.getResults().getNumFound() == 1);
-//        
-//        
-//        
-//        
-//        
-//        
-//        
-//        
-//        SolrQueryRequest req= new SolrQueryRequest();
-//        
-//        String queryText = "Hellow World";
-//        SolrIndexSearcher searcher = req.getSearcher();
-//        IndexReader reader = searcher.getAtomicReader();
-//        Terms terms = reader.getTermVector(0, "content_srch");
-//
-//        
-//        
-//        Map<String, Integer> termFreqMapsq = sensequery.getTermFreqMapfromTokenStream(getAnalyzer().tokenStream("", new StringReader(queryText)));
-//
-//        Map<String, Integer> termFreqMapssp = sensescoreprovider.getTermFreqmapfromTerms(terms);
-//
-//
-//
-
-
-        //do some assert here
-
     }
 
-    @Ignore
+    @Test
     public void testCKBVector() {
         Map<String, Integer> tf = new HashMap<String, Integer>();
         tf.put("hello", 1);
         tf.put("world", 1);
-        DoubleFullVector vector = ckb.getFullCkbVector(tf);
+        DoubleFullVector vector = CKB.getFullCkbVector(tf);
         System.out.println("Got vector of dimension: " + vector.getDimension());
         assertTrue("Vector has null dimention", vector.getDimension() > 0);
-        assertTrue("Vector has different dim than CKB", vector.getDimension() == ckb.getDimentionality());
+        assertTrue("Vector has different dim than CKB", vector.getDimension() == CKB.getDimentionality());
 
         for (double d : vector.getData()) {
             System.out.print(d + ", ");
